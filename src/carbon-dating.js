e33a0a2aa619da39514708b9fbb61f358d0c6b6f
yusivacova <sivacovayulia@gmail.com>
2023-04-13 21:50:33 +0300

    feat: add decision carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+    if (!sampleActivity || isNaN(sampleActivity)) return false;
+    if (sampleActivity < 0) return false;
+    if (typeof sampleActivity != 'string') return false;
+
+    let age = Math.log2((MODERN_ACTIVITY / +sampleActivity)) * HALF_LIFE_PERIOD;
+
+    if (!isFinite(age)) return false;
+    if (age < 0) return false;
+    
+
+    return Math.ceil(age);
 }
 
 module.exports = {
-  dateSample
+    dateSample
 };
