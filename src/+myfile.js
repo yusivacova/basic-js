44e150df1ee6d468a01b0235089e7a8ae4a22145
yusivacova <sivacovayulia@gmail.com>
2023-04-16 10:40:50 +0300

    fead: add solution sum-digits task

--- src/+myfile.js
+++ src/+myfile.js
@@ -1,61 +1,14 @@
-function deleteDigit(n) {
+function getSumOfDigits(n) {
     let arrN = n.toString().split('');
 
-    let arrDigitN = [];
+    let result = arrN.reduce((sum, current) => sum + +current, 0);
 
-    for (let i = 0; i < arrN.length; i++){
-        let next = arrN[i + 1];
-        let prev = arrN[i - 1];
-
-        if (i) {
-            if (prev < arrN[i]) {
-                arrN[i - 1] = '';
-                arrDigitN.push(arrN.join(''));
-                arrN[i - 1] = prev;
-            }
-
-            if (next > arrN[i]){
-                let current = arrN[i];
-                arrN[i] = '';
-                arrDigitN.push(arrN.join(''));
-                arrN[i] = current;
-            } 
-        }
+    if (result >= 10){
+        result = getSumOfDigits(result);
     }
 
-   // if (!arrDigitN.length) return n;
-    return +(arrDigitN.join(''));
+    return result;
 }
 
-console.log(deleteDigit(222219))
-//, 22229);
-//console.log(deleteDigit(109))
-//, 19);
-console.log(deleteDigit(342))
-//, 42);
-
-//console.log(deleteDigit(1001))
-//, 101);)
-console.log(deleteDigit(10))
-//, 1);
-
-
-/*
-function deleteDigit(n) {
-    let arrN = n.toString().split('');
-    const minOne = Math.min(...arrN).toString();
-    let index = arrN.indexOf(minOne);
-    arrN[index] = '';
-   
-    let firstNumber = Number(arrN.join(''));
-    arrN[index] = 10;
-    const minTwo = Math.min(...arrN).toString();
- 
-    arrN[index] = minOne;
-    index = arrN.indexOf(minTwo);
-    arrN[index] = '';
-    let secondNumber = Number(arrN.join(''));
-
-    return Math.max(firstNumber, secondNumber);
-}
-*/
+console.log(getSumOfDigits(99))
+//, 9);
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+    let arrN = n.toString().split('');
+
+    let result = arrN.reduce((sum, current) => sum + +current, 0);
+
+    if (result >= 10) {
+        result = getSumOfDigits(result);
+    }
+
+    return result;
 }
 
 module.exports = {
