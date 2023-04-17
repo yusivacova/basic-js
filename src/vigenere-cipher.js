07c2bcbf66b5dc0fdd86164103ce9c93d634ec82
yusivacova <sivacovayulia@gmail.com>
2023-04-18 00:03:09 +0300

    feat: add solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,81 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(value = true) {
+        this.value = value;
+    }
+
+    base = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+    encrypt(message, key) {
+        if (!message || !key) {
+            throw new Error('Incorrect arguments!');
+        }
+
+        let regexp = /^\p{L}$/u;
+
+        let result = [];
+
+        let j = 0;
+
+        for (let i = 0; i < message.length; i++) {
+            if (regexp.test(message[i])) {
+                let indexInTableMessage = this.base.indexOf(message[i].toUpperCase());
+
+                if (j > key.length - 1) {
+                    j = 0;
+                }
+
+                let indexInTableKey = this.base.indexOf(key[j].toUpperCase());
+
+                let sumIndexMessageAndKey = indexInTableMessage + indexInTableKey;
+
+                let indexEncrypt = (sumIndexMessageAndKey >= 26) ? sumIndexMessageAndKey % 26 : sumIndexMessageAndKey;
+                result.push(this.base[indexEncrypt]);
+                j++;
+            } else {
+                result.push(message[i]);
+            }
+        }
+
+        if (!this.value) return result.reverse().join('');
+        return result.join('');
+    }
+
+    decrypt(encryptedMessage, key) {
+        if (!encryptedMessage || !key) {
+            throw new Error('Incorrect arguments!');
+        }
+        let regexp = /^\p{L}$/u;
+
+        let result = [];
+
+        let j = 0;
+
+        for (let i = 0; i < encryptedMessage.length; i++) {
+            if (regexp.test(encryptedMessage[i])) {
+                let indexInTableMessage = this.base.indexOf(encryptedMessage[i].toUpperCase());
+
+                if (j > key.length - 1) {
+                    j = 0;
+                }
+
+                let indexInTableKey = this.base.indexOf(key[j].toUpperCase());
+
+                let differenceIndexMessageAndKey = indexInTableMessage - indexInTableKey;
+
+                let indexEncrypt = (differenceIndexMessageAndKey < 0) ? differenceIndexMessageAndKey + 26 : differenceIndexMessageAndKey;
+
+                result.push(this.base[indexEncrypt]);
+                j++;
+            } else {
+                result.push(encryptedMessage[i]);
+            }
+        }
+
+        if (!this.value) return result.reverse().join('');
+        return result.join('');
+    }
 }
 
 module.exports = {
